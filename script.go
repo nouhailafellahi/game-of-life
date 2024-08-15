f4737a16384b2712db72f7e0502b78309d4f0843
Nouhaila <nouhaila6fellahi@gmail.com>
2024-08-15 17:30:32 -0400

    bare bones version of GUI

--- script.go
+++ script.go
@@ -1,18 +1,22 @@
-package main 
+package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+    "fyne.io/fyne/v2/app"
 )
 
-
 func main() {
+	//Game of life.
+	//Once I am able to work the GUI for this program, it will have the following features: 
+	//	1. Two buttons: Play + Stop + Reset.
+	//	2. A clickable screen with many squares...? I do not know how to name it.
+	//The Play button will do the same thing whether the state of the program has just been reset or not.
+	//The Stop button will pause the operations of the program. Basically just freeeze it.
+	//The buttons Play and Stop are two sides of the same button LOL.
+	//The Reset button can be callled whenever needed. If called while game is playing. The Stop function will be
+	//called then all instance variable values and screen will be reset. If called after the Stop button is called, 
+	// only the second part will happen.
+
 	a := app.New()
 	w := a.NewWindow("Conway's Game of Life")
-
-
-	w.setContent(widget.NewLabel("Hello..."))
 	w.ShowAndRun()
-
 }
