08a7a76f0507be90c47462506148b89d073f6175
Nouhaila <nouhaila6fellahi@gmail.com>
2024-08-16 16:45:19 -0400

    Clickable grid + Window Icon

--- script.go
+++ script.go
@@ -4,10 +4,11 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	//"fyne.io/fyne/v2/layout"
     "fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2"
 	"image/color"
-	"fmt"
+	"log"
 )
 
 //array of squares
@@ -15,22 +16,55 @@ import (
 //valye of 1 if colored
 var squares [25][50]int
 
+//Struct and declaration of a clickable rectangle. Used as the background of grid
+type clickableRectangle struct {
+	widget.BaseWidget
+	rect    *canvas.Rectangle
+	OnTapped func(fyne.Position)
+}
 
-func play(grid *fyne.Container) {
-	for i:=0; i<len(squares); i++{
-		for j:=0; j<len(squares[i]); j++ {
-			rect := canvas.NewRectangle(color.White)
-			rect.Resize(fyne.NewSize(20,20))
-			rect.Move(fyne.NewPos(float32(j*20), float32(i*20)))
-			grid.Add(rect)	
-		}	
+func newClickableRectangle() *clickableRectangle {
+	r := &clickableRectangle{
+		rect: canvas.NewRectangle(color.White),
 	}
-	
+	r.ExtendBaseWidget(r)
+	return r
+}
+
+func (r *clickableRectangle) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(r.rect)
+}
+
+func (r *clickableRectangle) Tapped(event *fyne.PointEvent) {
+	if r.OnTapped != nil {
+		r.OnTapped(event.Position)
+	}
+}
+
+//Methods
+func play(grid *fyne.Container) {
+
 	
 }
 
+func stop(grid *fyne.Container) {
+
+}
+
+func reset(grid *fyne.Container) {
+	stop(grid)
+
+}
+
+func click(grid *fyne.Container, pos fyne.Position){
+	println("Position clicked: ", pos.X, pos.Y)//debug
+}
+
+
+
+//Main
 func main() {
-	fmt.Println("Start build")//debug
+	println("Start build")//debug
 	a := app.New()
 	w := a.NewWindow("Conway's Game of Life")
 	
@@ -61,12 +95,23 @@ func main() {
 			rect.Move(fyne.NewPos(0, float32(20*i)))
 		}
 	}
+
 	
 
-	//Properly size+position grid and add to window
+	//Properly size grid 
 	grid.Resize(fyne.NewSize(1000,500))
-	content := container.NewWithoutLayout(grid)
-	grid.Move(fyne.NewPos(100,50))
+
+	//Create a clickable, transparent screen behind the grid
+	screen := newClickableRectangle()
+	screen.OnTapped = func(pos fyne.Position) {
+		click(grid, pos)
+	}
+	screen.Resize(fyne.NewSize(1000,500))
+	screen.rect.FillColor = color.RGBA{R:100,G:100,B:100,A:30}
+	
+	content := container.NewWithoutLayout(screen, grid)
+	grid.Move(fyne.NewPos(100,40))
+	screen.Move(fyne.NewPos(100,40))
 
 
 	//Create and position Play button
@@ -79,42 +124,44 @@ func main() {
 	content.Add(playBtn)
 
 
+
 	//Create and position Stop button
 	stopBtn := widget.NewButton("Stop", func(){
-		stop()
+		stop(grid)
 	} )
 	stopBtn.Move(fyne.NewPos(525, 580))
 	stopBtn.Resize(fyne.NewSize(150, 50))
 	//Add button to window
 	content.Add(stopBtn)
 
+
+
 	//Create and position Reset button
 	resetBtn := widget.NewButton("Reset", func(){
-		reset()
+		reset(grid)
 	} )
 	resetBtn.Move(fyne.NewPos(750, 580))
 	resetBtn.Resize(fyne.NewSize(150, 50))
 	//Add button to window
 	content.Add(resetBtn)
 	
-
 	w.SetContent(content)
 	w.Resize(fyne.NewSize(1200,670))
+
+	//Use customized window icon
+	icon , err := fyne.LoadResourceFromPath("./cat.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.SetIcon(icon)
+
+
+
 
 	go func() {
 		//run while show and running
 	}()
 
 	w.ShowAndRun()	
-
-}
-
-func stop() {
-
-}
-
-func reset() {
-	stop()
-
 
 }
