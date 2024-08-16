b21e383c1c403486cc6bcfb2474ac2090740cadf
Nouhaila <nouhaila6fellahi@gmail.com>
2024-08-15 23:07:57 -0400

    Render horizontal lines

--- script.go
+++ script.go
@@ -7,6 +7,7 @@ import (
     "fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2"
 	"image/color"
+	"fmt"
 )
 
 func main() {
@@ -14,25 +15,28 @@ func main() {
 	w := a.NewWindow("Conway's Game of Life")
 	
 
-	grid := container.NewGridWithColumns(22)
+	grid := container.NewWithoutLayout()
 
-	for i := 0; i < 572; i++ {
-		rect := canvas.NewRectangle(color.Black)
-		rect.Resize(fyne.NewSize(20,20))
+	//draw horizontal lines
+	for i := 0; i < 20; i++ {
+		rect := canvas.NewRectangle(color.White)
+
+		rect.Resize(fyne.NewSize(1000,1))
 		rect.FillColor = color.White;
 		rect.StrokeColor = color.White
-		rect.StrokeWidth = 1
-		grid.Add(rect)
 
+		grid.Add(rect)
+		rect.Move(fyne.NewPos(0, float32(30*i)))
 	}
-	grid.Resize(fyne.NewSize(500,600))
+	
+	grid.Resize(fyne.NewSize(1000,600))
 
 	content := container.NewWithoutLayout(grid)
 	grid.Move(fyne.NewPos(100,0))
 
 	w.SetContent(content)
 
-	w.Resize(fyne.NewSize(700,670))
+	w.Resize(fyne.NewSize(1200,670))
 	w.ShowAndRun()	
 
 }
